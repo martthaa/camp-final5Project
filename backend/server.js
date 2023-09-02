fbfb2173cf471cbdfd34e9d7b860bf2e6df97f5c
Martha <martasobotyk@gmail.com>
2023-09-02 18:59:29 +0300

    fix bug

--- backend/server.js
+++ backend/server.js
@@ -22,10 +22,6 @@ app.use(express.urlencoded({ extended: true }));
 // Cokie parser middleware
 app.use(cookieParser());
 
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -39,17 +35,19 @@ const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 
 if (process.env.NODE_ENV === 'production') {
-    // set static folder
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
-    
-    // any route that is not api will be redirected to index.html
-    app.get('*', (req, res) => 
-      res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
+  const __dirname = path.resolve();
+  app.use('/uploads', express.static('/var/data/uploads'));
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
 } else {
+  const __dirname = path.resolve();
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
   app.get('/', (req, res) => {
     res.send('API is running...');
-  })
+  });
 }
 
 app.use(notFound);
